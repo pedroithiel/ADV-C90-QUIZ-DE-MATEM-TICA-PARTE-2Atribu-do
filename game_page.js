26f1bb0e81ea54493feb34fafcd8269cc613a10b
pedroithiel <101153331+pedroithiel@users.noreply.github.com>
2022-08-26 14:59:15 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,24 @@
+var resposta = document.getElementById("conta").innerHTML = primeiroNumero * segundoNumero;
+function Enviar() {
+    
+    var primeiroNumero = document.getElementById("primera").value;
+    var segundoNumero = document.getElementById("segunda").innerHTML.value;
+   
+    
+}
+
+function calculo() {
+    Enviar()
+    
+}
+
+function Verificar() {
+    calculo()
+    if(resposta == "conta") {
+        
+    }
+
+}
+
+
+
